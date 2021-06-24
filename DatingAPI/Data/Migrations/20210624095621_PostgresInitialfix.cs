5658b4d039f717e1a1abefdc8b58c23a9d169c2f
kavithaswin <6270029+kavithaswin@users.noreply.github.com>
2021-06-24 16:48:13 +0530

    Deployment fix

---
+++ DatingAPI/Data/Migrations/20210624095621_PostgresInitialfix.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatingAPI.Data.Migrations
+{
+    public partial class PostgresInitialfix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
